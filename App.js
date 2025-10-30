0a429739e24f4dcf13a8994073a488825088b3f6
Daniel Alexandro Castelo Castillo <124051585@upq.edu.mx>
2025-10-29 22:25:02 -0600

    Comentarios

--- App.js
+++ App.js
@@ -47,8 +47,8 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         {/* Inicia con el login */}
-        <Stack.Screen name="Login-" component={LoginScreen} />
-        <Stack.Screen name="Register-" component={RegisterScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />// Pantalla de login
+        <Stack.Screen name="Register" component={RegisterScreen} />// Pantalla de registro
         {/* Cuando el usuario inicia sesión */}
         <Stack.Screen name="MainTabs" component={MainTabs} />
       </Stack.Navigator>
